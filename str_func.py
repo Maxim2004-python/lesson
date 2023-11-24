ac24db40b495ebb7c950590c9b60d8f7b0ea20ae
Maxim <maxkolobov2004.09v@gmail.com>
2023-11-24 14:47:29 +0500

    .

--- str_func.py
+++ str_func.py
@@ -1,9 +1,20 @@
 def str_func():
+    ''' Эта функция для преобразовнаия слов'''
 
     words = input()
 
     cap = words.upper()
 
     return cap
+
+
+def words_upper(sentence):
+
+    '''Эта функция для преобразования слов'''
+
+    return sentence.title()
+
+print(words_upper("я упал со стула"))
+
 
 
